2e2053ab5332ccdc8b5d99fd40bfda71e309b28a
stephenogidiagba <153740890+stephenogidiagba@users.noreply.github.com>
2023-12-13 22:42:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['917605902011', 'Steven', true],
+  ['6281255369012', 'Brume', true], 
   [''] 
 ] //Number of owners
 
